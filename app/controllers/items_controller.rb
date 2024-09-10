27c1cd7d919d62105039e9fb48296aa82d219043
mattsu111 <songjingjianwu5@gmail.com>
2024-09-10 20:59:37 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update ]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -40,6 +40,12 @@ end
     
     end
   end  
+  
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+  
 
   private
   
