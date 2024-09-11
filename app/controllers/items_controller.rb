c4239504d2576b9122a5ae0f7279f8a502412788
mattsu111 <songjingjianwu5@gmail.com>
2024-09-11 20:22:22 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
-
+  before_action :correct_user, only: [:destroy]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -40,7 +40,7 @@ end
     
     end
   end  
-  
+
   def destroy
     @item.destroy
     redirect_to root_path
@@ -56,6 +56,10 @@ end
   def set_item
     @item = Item.find(params[:id])
   end  
+
+  def correct_user
+    redirect_to items_path, alert: '権限がありません。' unless current_user.id == @item.user_id
+end
 
   def move_to_index
     unless current_user.id == @item.user_id
