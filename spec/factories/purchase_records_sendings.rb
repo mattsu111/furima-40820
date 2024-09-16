ac834c0229db6c47149083f22a60f4c980e7504e
mattsu111 <songjingjianwu5@gmail.com>
2024-09-16 17:30:01 +0900

    修正３

--- spec/factories/purchase_records_sendings.rb
+++ spec/factories/purchase_records_sendings.rb
@@ -7,7 +7,6 @@ FactoryBot.define do
     building { 'BuildingName' }
     telephone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
-    association :user
-    association :item
+
   end
 end
